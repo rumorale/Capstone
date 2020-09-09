8bc96f3c433d5e88fbb64eee08644acb268e79ad
rmSTS <rodrigo.morales@stratagemmasys.com>
2020-09-09 16:02:12 -0300

    warnings and notes corrected

--- R/functions.R
+++ R/functions.R
@@ -4,6 +4,8 @@
 #'
 #' @param x vector to transform to numeric
 #'
+#' @importFrom dplyr "%>%"
+#'
 #' @return numeric vector
 #'
 #' @examples
@@ -19,6 +21,8 @@ trimws_to_numeric = function(x){ trimws(x) %>% as.numeric()}
 #' More detailed description
 #'
 #' @param LOCATION_NAME vector that include country and location in the same string
+#'
+#' @importFrom dplyr "%>%"
 #'
 #' @return vector that contains only location
 #'
@@ -44,6 +48,9 @@ eq_location_clean <- function(LOCATION_NAME){
 #'
 #' @param Data Earthquake data
 #'
+#' @importFrom tidyr "%>%"
+#' @importFrom dplyr mutate
+#'
 #' @return Earthquake data Clean
 #'
 #' @examples
@@ -54,7 +61,7 @@ eq_location_clean <- function(LOCATION_NAME){
 eq_clean_data <- function(Data){
 
         Data%>%
-                dplyr::mutate(MONTH = ifelse(is.na(MONTH),0, MONTH),
+                dplyr::mutate(MONTH = ifelse(is.na(get(MONTH)),0, get(MONTH)),
                               DAY = ifelse(is.na(DAY),0, DAY),
                               DEATHS = as.numeric(DEATHS),
                               EQ_PRIMARY = as.numeric(EQ_PRIMARY)) %>%
@@ -70,3 +77,155 @@ eq_clean_data <- function(Data){
 
 }
 
+
+
+#' Plot timeline earthquakes
+#'
+#' More detailed description
+#'
+#' @return Earthquake data Clean
+#' @rdname ggplot2-ggproto
+#' @format NULL
+#' @usage NULL
+#' @export
+GeomTimeline <- ggplot2::ggproto("GeomTimeline", ggplot2::Geom,
+                        required_aes = c("x"),
+                        default_aes = ggplot2::aes(shape = 20, color = "black", size = 1, alpha = 0.6),
+                        draw_key = ggplot2::draw_key_point,
+                        draw_panel = function(data, panel_scales, coord) {
+
+                                coords <- coord$transform(data, panel_scales)
+
+                                if(is.null(coords$y)){ coords$y  = 0.3}
+
+                                points <- grid::pointsGrob(x = coords$x,
+                                                           y = coords$y,
+                                                           size = grid::unit(coords$size, "mm"),
+                                                           pch = coords$shape,
+                                                           gp = grid::gpar(alpha = coords$alpha,
+                                                                           col = coords$colour))
+
+
+                                coords$xmin = min(coords$x)
+                                coords$xmax = max(coords$x)
+
+                                lower <- grid::segmentsGrob(x0 = coords$xmin,
+                                                            x1 = coords$xmax,
+                                                            y0 = coords$y,
+                                                            y1 = coords$y,
+                                                            gp = grid::gpar(alpha = 0.6,
+                                                                            lwd = 1,
+                                                                            col = "grey60"))
+
+                                base <- grid::segmentsGrob(x0 = 0,
+                                                           x1 = 1,
+                                                           y0 = 0,
+                                                           y1 = 0,
+                                                           gp = grid::gpar(alpha = coords$alpha))
+
+                                grid::gTree(children = grid::gList(points, lower, base))
+
+                        }
+)
+
+#' Plot timeline earthquakes
+#'
+#' More detailed description
+#'
+#' @param mapping Set of aesthetic mappings created by aes() or aes_()
+#' @param data The data to be displayed in this layer. There are three options:
+#'         If NULL, the default, the data is inherited from the plot data as specified in the call to ggplot().
+#'         data.frame, or other object, will override the plot data. All objects will be fortified to produce a data frame. See
+#'         fortify() for which variables will be created.
+#'         A function will be called with a single argument, the plot data.
+#'         The return value must be a data.frame, and will be used as the layer data. A function can be created from a formula (e.g. ~ head(.x, 10)).
+#' @param stat stat parameter
+#' @param position position parameter
+#' @param show.legend show.legend parameter
+#' @param na.rm na.rm parameter
+#' @param inherit.aes  inherit.aes parameter
+#' @param ... other parameters
+#'
+#' @return Earthquake data Clean
+#'
+#' @examples
+#' \dontrun{
+#' eq_clean_data(Data)
+#' }
+#' @export
+geom_timeline <- function(mapping = NULL, data = NULL, stat = "identity",
+                          position = "identity", show.legend = NA,
+                          na.rm = FALSE, inherit.aes = TRUE, ...) {
+        layer(
+                data = data,
+                mapping = mapping,
+                stat = stat,
+                geom = GeomTimeline,
+                position = position,
+                show.legend = show.legend,
+                inherit.aes = inherit.aes,
+                params = list(na.rm = na.rm, ...)
+        )
+}
+
+#' @rdname ggplot2-ggproto
+#' @format NULL
+#' @usage NULL
+#' @export
+GeomTimelineLabel <- ggplot2::ggproto("GeomTimelineLabel", ggplot2::Geom,
+                             required_aes = c("x", "label"),
+                             default_aes = ggplot2::aes(n_max = 10),
+                             draw_key = ggplot2::draw_key_point,
+                             draw_panel = function(data, panel_scales, coord) {
+                                     coords <- coord$transform(data, panel_scales)
+                                     # print(coords)
+                                     if(is.null(coords$y)){ coords$y  = 0.3}
+                                     if(is.null(coords$n_max)){ coords$n_max  = 10}
+
+
+                                     n_max = coords$n_max[1]
+
+                                     coords_n_max <- coords %>% group_by(y) %>% top_n(n=n_max[1], wt = size)
+
+                                     text_HQ <- grid::textGrob(label = coords_n_max$label,
+                                                               x = coords_n_max$x,
+                                                               y = coords_n_max$y*1.1,vjust = 0, hjust = 0, rot = 45)
+
+                                     lines_HQ <- grid::segmentsGrob(x0 = coords_n_max$x,
+                                                                    y0 = coords_n_max$y,
+                                                                    x1 = coords_n_max$x,
+                                                                    y1 = coords_n_max$y*1.1,
+                                                                    gp = grid::gpar(alpha = 0.6,
+                                                                                    lwd = 1,
+                                                                                    col = "grey60")
+                                     )
+
+                                     grid::gTree(children = grid::gList(text_HQ, lines_HQ))
+                             }
+)
+
+#' Plot timeline earthquakes
+#'
+#' More detailed description
+#'
+#' @inheritParams geom_timeline
+#'
+#' @examples
+#' \dontrun{
+#' eq_clean_data(Data)
+#' }
+#' @export
+geom_timelinelabel <- function(mapping = NULL, data = NULL, stat = "identity",
+                               position = "identity", show.legend = NA,
+                               na.rm = FALSE, inherit.aes = TRUE, ...) {
+        layer(
+                data = data,
+                mapping = mapping,
+                stat = stat,
+                geom = GeomTimelineLabel,
+                position = position,
+                show.legend = show.legend,
+                inherit.aes = inherit.aes,
+                params = list(na.rm = na.rm, ...)
+        )
+}
---
+++ R/global.R
@@ -0,0 +1,3 @@
+utils::globalVariables(c("MONTH", "DAY", "DEATHS", "EQ_PRIMARY",
+                         "ymd", "years", "YEAR", "days", "DATE", "LATITUDE", "LONGITUDE",
+                         "LOCATION_NAME", "layer"))
